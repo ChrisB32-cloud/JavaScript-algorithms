5a28adfffe16593efbf8912ffe432ccb968012b0
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-06-09 18:54:12 -0500

    persistence problem

--- app.js
+++ app.js
@@ -252,3 +252,49 @@ function pigIt(str) {
 }
 
 pigIt(str)
+
+
+
+// function pigIt(str){
+//     const arrStr = str.split(' ');
+//     let newArr = [];
+//       for(let i = 0; i < arrStr.length; i++) {
+//         let getArrAlph = `${arrStr[i][0]}ay`;
+//         let removeFChar = arrStr[i].split('').slice(1, arrStr[i].length).join('');
+//         let concatStr = `${removeFChar}${getArrAlph}`;
+//         newArr.push(concatStr);
+//       }
+//     return newArr.join(' ')
+//   }
+
+
+// Write a function, persistence, that takes in a positive parameter 
+// num and returns its multiplicative persistence, which is the 
+// number of times you must multiply the digits in num until you reach a single digit.
+
+// For example:
+
+// persistence(39) === 3 
+// because 3*9 = 27, 2*7 = 14, 1*4=4
+// and 4 has only one digit
+
+// persistence(999) === 4 
+// because 9*9*9 = 729, 7*2*9 = 126,
+// 1*2*6 = 12, and finally 1*2 = 2
+
+// persistence(4) === 0 
+// because 4 is already a one-digit number
+
+const num = 39
+
+function persistence(num) {
+    if (num >= 0 && num <= 9) {
+        return 0
+    }
+    let numArr = Array.from(num.toString()).map(Number);
+    for (let i = 0; i < numArr.length; i++) {
+
+    }
+}
+
+persistence(num)
