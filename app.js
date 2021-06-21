fc7881d3865d9bea8f73e6917d2edb5f464f41d3
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-06-21 14:42:48 -0500

    Product of sequenal fib numbers

--- app.js
+++ app.js
@@ -362,5 +362,75 @@ function persistence_1(num) {
 // persistence_1(num)
 
 
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
 
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
 
+// such as
+
+// F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+// Some Examples of Return:
+
+// productFib(714) # should return (21, 34, true), 
+//                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+// productFib(800) # should return (34, 55, false), 
+//                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+// -----
+// productFib(714) # should return [21, 34, true], 
+// productFib(800) # should return [34, 55, false], 
+// -----
+// productFib(714) # should return {21, 34, 1}, 
+// productFib(800) # should return {34, 55, 0},        
+// -----
+// productFib(714) # should return {21, 34, true}, 
+// productFib(800) # should return {34, 55, false},
+
+const prod = 5895
+
+function productFib(prod) {
+    let arrFib = [];
+    arrFib[0] = 0;
+    arrFib[1] = 1;
+    let count = 0
+    let k = 1
+    for (let i = 2; i <= 200; i++) {
+        arrFib[i] = arrFib[i - 2] + arrFib[i - 1];
+    }
+    for (let j = 0; j <= 200; j++) {
+        count = arrFib[j] * arrFib[k]
+        if (count === prod) {
+            return [arrFib[j], arrFib[k], true]
+        }
+        if (count > prod) {
+            return [arrFib[j], arrFib[k], false]
+        }
+        k++
+    }
+
+    // console.log(newFibArr)
+}
+
+productFib(prod)
+
+// count += i + j
+        // arrFib.push(count)
+        // j++
+
+// arrFib.push(arrFib[i - 2] + arrFib[i - 1])
+        // console.log(arrFib)
