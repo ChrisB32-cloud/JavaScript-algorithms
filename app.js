a9b95dc018e9519011b58985bd067cca60655b5d
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-07-23 10:25:48 -0500

    square sum algo

--- app.js
+++ app.js
@@ -700,3 +700,22 @@ function digitize(n) {
 }
 
 digitize(z)
+
+
+// Complete the square sum function so that it squares each number 
+// passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 
+// 9 because 1^2 + 2^2 + 2^2 = 9.
+
+const numPass = [0, 3, 4, 5]
+
+function squareSum(numbers) {
+    let squareCount = 0
+    for (let i = 0; i < numbers.length; i++) {
+        squareCount += numbers[i] ** 2
+    }
+    return squareCount
+}
+
+squareSum(numPass)
