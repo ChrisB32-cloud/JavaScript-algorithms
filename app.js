bff962de7517c6ad316a209cb0303414b203cdbe
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-07-27 16:36:14 -0500

    Workers day algo

--- app.js
+++ app.js
@@ -770,3 +770,47 @@ function isLeapYear(year) {
 }
 
 isLeapYear(determineLeapYear)
+
+// There are five workers : James,John,Robert,Michael and William.They work one by one and on weekends 
+// they rest. Order is same as in the description(James works on mondays,John works on 
+// tuesdays and so on).You have to create a function 'task' that will take 3 arguments(w, n, c):
+
+// Weekday
+
+// Number of trees that must be sprayed on that day
+
+// Cost of 1 litre liquid that is needed to spray tree,let's say one tree needs 1 litre liquid.
+
+// Let cost of all liquid be x
+
+// Your function should return string like this : 'It is (weekday) today, (name), you have to work, 
+// you must spray (number) trees and you need (x) dollars to buy liquid'
+
+// For example:
+// task('Monday', 15, 2) -> 'It is Monday today, James, you have to work, you 
+// must spray 15 trees and you need 30 dollars to buy liquid'
+
+function task(w, n, c) {
+    const workers = ['James', 'John', 'Robert', 'Michael', 'William']
+    let x = n * c
+    for (let i = 0; i < workers.length; i++) {
+        if (w === 'Monday') {
+            return `It is ${w} today, James, you have to work, you must spray ${n} trees and you need ${x} dollars to buy liquid`
+        }
+        if (w === 'Tuesday') {
+            return `It is ${w} today, John, you have to work, you must spray ${n} trees and you need ${x} dollars to buy liquid`
+        }
+        if (w === 'Wednesday') {
+            return `It is ${w} today, Robert, you have to work, you must spray ${n} trees and you need ${x} dollars to buy liquid`
+        }
+        if (w === 'Thursday') {
+            return `It is ${w} today, Michael, you have to work, you must spray ${n} trees and you need ${x} dollars to buy liquid`
+        }
+        if (w === 'Friday') {
+            return `It is ${w} today, William, you have to work, you must spray ${n} trees and you need ${x} dollars to buy liquid`
+        }
+    }
+
+}
+
+task('Tuesday', 20, 4)
